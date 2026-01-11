36dddd3c5d6cd5def8ea649d0d85e3724cb7b412
bima1313 <bimairfy13@gmail.com>
2026-01-11 13:39:28 +0700

    refactor: reorganize customer service test into dedicated sub-folder

--- src/test/java/com/project/foodmarket/food_management/customer/service/CustomerServiceTest.java
+++ src/test/java/com/project/foodmarket/food_management/customer/service/CustomerServiceTest.java
@@ -1,4 +1,4 @@
-package com.project.foodmarket.food_management.service;
+package com.project.foodmarket.food_management.customer.service;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
